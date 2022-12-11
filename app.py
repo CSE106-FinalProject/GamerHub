db2608e3c217ebf8362d54c7bdd6972f351a0e71
Rodrigo Martinez <rodrigomartinez1477@gmail.com>
2022-12-10 20:38:55 -0800

    deleted comments

--- app.py
+++ app.py
@@ -11,20 +11,6 @@ with app.app_context():
     db.create_all()
 
 
-# class Owner(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(20))
-#     address = db.Column(db.String(100))
-#     pets = db.relationship('Pet', backref='owner')
-
-
-# class Pet(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(20))
-#     age = db.Column(db.Integer)
-#     owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
-
-
 class User(db.Model):
     """
     User Database
